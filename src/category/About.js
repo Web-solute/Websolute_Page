d854c32e8119b60c1a959639f18bea9d8963a4fe
Lee Ji Seok <76767226+dev-jiseok@users.noreply.github.com>
2022-01-20 17:48:11 +0900

    중간커밋

--- src/category/About.js
+++ src/category/About.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Footer from "../Footer";
 import Menu from "../Menu";
+import './About.css';
 
 class About extends Component {
   /*
@@ -15,8 +16,65 @@ class About extends Component {
   render() {
     return (
       <>
-        <Menu />
-        About
+        <div className="background">
+          <Menu />
+          <div className="slogan">
+
+            <h1 className="slogan_text">
+              Websolute는 IT 업계를 주도하는
+              개발자와 디자이너를 위한 모임입니다.<br /><br />
+
+              자유롭게 협업하고 소통하며,
+              자기역량 강화와<br /> 새로운 서비스 경험을 통해
+              IT 인재로 발전하는 것을 목표로 합니다.
+            </h1>
+          </div>
+        </div>
+
+        <div className="infoTable">
+          <h2 className="title1">
+            2년째 <br />멈추지 않는 열정!
+          </h2>
+
+          <div className="infomation">
+            <div className="card">
+              <div className="card-info">
+                <div className="contents">
+                  <h4 className="title">운영기간</h4>
+                  <h2 className="value">2년</h2>
+                  <p className="description">2020년 부터</p>
+                </div>
+              </div>
+              <div className="card-info">
+                <div className="contents">
+                  <h4 className="title">런칭서비스</h4>
+                  <h2 className="value">5+</h2>
+                  <p className="description">기수별 런칭율 100%</p>
+                </div>
+              </div>
+              <div className="card-info">
+                <div className="contents">
+                  <h4 className="title">가입 회원수</h4>
+                  <h2 className="value">7명</h2>
+                  <p className="description">신규 기수마다 약 2명씩 증가</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <br />
+          <br />
+
+          <div className="activity">
+            <h2 className="title1">
+              함께하는 <br />다양한 활동!
+            </h2>
+            <h3>정규 활동은 매주 월요일에 회의가 진행되며, 프로젝트를 진행합니다. <br />
+              비활동 기간에는 프로젝트·공모전·스터디를 자율적으로 진행합니다.</h3>
+          </div>
+        </div>
+
         <Footer />
       </>
     );
